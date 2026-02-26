c85acfdca99860943cf412a347b16c5b97b4a020
Matiastamayo19 <mtamayog3@eafit.edu.co>
2026-02-26 10:05:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 import streamlit as st
 from PIL import Image
+
+st.title("Mi primera app")
+st.header("En este espacio cominezo a desarrollar mis aplicaciones para interfaces multimodales")
+st.write("Facilmente puedo realizar backend y frontend")
+Image = Image.open('')
