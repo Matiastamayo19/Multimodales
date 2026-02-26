da20533c554cfa45146236dae4226e06d784f8a5
Matiastamayo19 <mtamayog3@eafit.edu.co>
2026-02-26 10:09:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from PIL import Image
 st.title("Mi primera app")
 st.header("En este espacio cominezo a desarrollar mis aplicaciones para interfaces multimodales")
 st.write("Facilmente puedo realizar backend y frontend")
-Image = Image.open('image.jpeg')
+image = Image.open('image.jpeg')
 st.Image(image, caption='Interfaces Multimodales')
